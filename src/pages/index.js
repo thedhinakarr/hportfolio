ad2dd11554a4f086d63e1b47269db3bef3e13f46
thedhinakarr <codedhinakarr@gmail.com>
2024-03-21 18:31:14 +0530

    Hiptify Image ADdition

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
@@ -6,6 +7,10 @@ const IndexPage = () => {
       <div className="max-w-md w-full p-8 bg-white rounded-lg shadow-lg">
         <h1 className="text-3xl font-semibold text-gray-800 mb-4">Site Under Development</h1>
         <p className="text-gray-600 mb-6">Sorry, this page is currently under development. Please check back later!</p>
+        <StaticImage
+        src="../images/HI.png"
+        alt="Image 1"
+      />
       </div>
     </div> 
   )
