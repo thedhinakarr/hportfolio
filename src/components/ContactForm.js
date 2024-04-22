e1413b4fcf1123b3e7f69423b04f00a4c719066f
thedhinakarr <codedhinakarr@gmail.com>
2024-04-22 15:52:35 +0530

    Added form submission

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -19,7 +19,11 @@ const ContactForm = () => {
 
 
   return (
-    <form onSubmit={handleSubmit} name="contact" method="post" className=" border-dblue border-4 p-3 max-w-md mx-auto" data-netlify="true">
+    <form onSubmit={handleSubmit} name="contact" method="post" className="border-dblue border-4 p-3 max-w-md mx-auto" data-netlify="true">
+      {/* Add hidden form fields */}
+      <input type="hidden" name="form-name" value="contact" />
+
+      {/* Rest of your form */}
       <p>
         <label className="block mb-2">Your Name:</label>
         <input type="text" name="name" className="w-full border border-gray-300 rounded-md px-3 py-2" />
@@ -33,9 +37,10 @@ const ContactForm = () => {
         <textarea name="message" className="w-full border border-gray-300 rounded-md px-3 py-2"></textarea>
       </p>
       <p>
-        <button type="submit" className=" items-center justify-center bg-blue hover:bg-dblue text-white font-bold py-2 px-4 rounded">Send</button>
+        <button type="submit" className="items-center justify-center bg-blue hover:bg-dblue text-white font-bold py-2 px-4 rounded">Send</button>
       </p>
     </form>
+
   )
 };
 
