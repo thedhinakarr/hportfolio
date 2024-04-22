25470250b4de4b46e0da2022da8111393b352dcb
thedhinakarr <codedhinakarr@gmail.com>
2024-04-22 16:02:12 +0530

    Added form submission

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,3 +1,4 @@
+import { navigate } from 'gatsby';
 import React from 'react';
 
 const ContactForm = () => {
@@ -13,7 +14,7 @@ const ContactForm = () => {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(() => alert("Your input has been submitted. Thank you!"))
+      .then(() => { alert("Your input has been submitted. Thank you!"); navigate("/"); })
       .catch((error) => alert(error));
   };
 
